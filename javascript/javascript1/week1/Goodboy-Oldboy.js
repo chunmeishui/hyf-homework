75d715f27f7f3677dbd3abb614f877f6697c588b
chunmeishui <chunmeishui578659@gmail.com>
2021-10-25 21:44:31 +0200

    changed the console.log

--- javascript/javascript1/week1/Goodboy-Oldboy.js
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -1,12 +1,9 @@
-const yearOfBirth = 1987;
-const yearFuture = 2045;
-const age = yearFuture - yearOfBirth;
 const dogYearOfBirth = 2017;
-const dogYearFuture = 2027;
-const shouldShowResultInDogYears = true;
+let dogYearFuture = 2027;
+let shouldShowResultInDogYears = true;
 const dogYear = dogYearFuture - dogYearOfBirth;
-if (shouldShowResultInDogYears){
-    console.log("Your dog will be " + dogYear+" human years old in "+dogYearFuture+"!")
-}else{
-    console.log("Your dog will be 70 dog years old in " +" " + yearFuture+"!")
+if (shouldShowResultInDogYears) {
+    console.log("Your dog will be " + dogYear + " human years old in " + dogYearFuture + "!")
+} else {
+    console.log("Your dog will be" + " " + dogYear * 7 + " " + "years old in " + " " + dogYearFuture + " " + "!")
 }
