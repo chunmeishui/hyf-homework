722628f37221d570e9276a9f55d5987108f42ef8
chunmeishui <chunmeishui578659@gmail.com>
2021-12-15 21:49:35 +0100

    finished javascript3 week1 giphy search homework

---
+++ javascript/javascript3/week1/giphy.js
@@ -0,0 +1,39 @@
+const apiKey = "KzWbfntmytNcoeo9ygrJJdIstwy3bjtB";
+const giphyInput = document.getElementById("giphyInput");
+const giphyInputNumber = document.getElementById("giphyInputNumber");
+const searchGif = document.getElementById("searchGif");
+const imageResult = document.getElementById("imageResult");
+searchGif.addEventListener("click", imagesOutput);
+function imagesOutput() {
+    let searchName = giphyInput.value;
+    let searchNumber = giphyInputNumber.value;
+    if (searchName === "") {
+        alert("Please enter your key words first")
+    } else {
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchName}&limit=${searchNumber}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                const resultDataArray = data.data;
+                for (let i = 0; i < resultDataArray.length; i++) {
+                    const http = resultDataArray[i].images.downsized.url;
+                    const altCotent = resultDataArray[i].title;
+                    const Li = document.createElement("li");
+                    const image = document.createElement("img");
+                    //    image.style.width = "400px";
+                    //    image.style.height = "auto";
+                    image.src = http;
+                    image.alt = altCotent;
+                    Li.appendChild(image);
+                    imageResult.appendChild(Li);
+                    giphyInput.value = ""
+                    giphyInput.addEventListener("input", reload)
+                }
+            });
+    }
+}
+function reload() {
+    window.location.reload(true);
+}
+
+
